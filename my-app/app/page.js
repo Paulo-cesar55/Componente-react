c287720272b20e2e88a4bb73d589931d9e1742e2
Paulo Cesar <paulocesardejesusneto@gmail.com>
2026-03-13 02:53:45 +0000

    atualizar

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -4,7 +4,7 @@ import Profile from "./components/profile";
 export default function Home() {
   return (
     <main>
-      <h1>Site criado para a disciplina Programação Front-End</h1>
+      <h1>Site criado para a disciplina Programação Front-End 2026.1</h1>
       <Profile />
     </main>
   );
