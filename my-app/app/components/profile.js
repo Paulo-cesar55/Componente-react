e85bab6019af93ac2b4d8c3e421fb17e6701b15f
Paulo Cesar <paulocesardejesusneto@gmail.com>
2026-03-13 02:41:25 +0000

    Componente

---
+++ my-app/app/components/profile.js
@@ -0,0 +1,19 @@
+export default function Profile() {
+  return (
+    <div style={{ textAlign: "center" }}>
+       <img
+        src="/foto.jpeg"
+        
+      />
+
+      <h2>Paulo Cesar</h2>
+
+      <p>
+        Estudante Si terceiro periodo S2
+      </p>
+      <p>
+        "Todo dia procurando o melhor de mim mesmo"
+      </p>
+    </div>
+  );
+}
---
+++ my-app/app/page.js
@@ -0,0 +1,11 @@
+
+import Profile from "./components/profile";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Site criado para a disciplina Programação Front-End</h1>
+      <Profile />
+    </main>
+  );
+}
